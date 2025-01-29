cf8bd0089b4eaddf85d6bc6ade9bc9d9e9b876c9
Nick Wylynko <nickwylynko@clerk.dev>
2025-01-30 00:56:32 +0800

    init

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import { SignedIn, UserProfile } from "@clerk/nextjs";
+
+export default function Home() {
+  return (
+    <main>
+      <SignedIn>
+        <UserProfile routing="hash" />
+      </SignedIn>
+    </main>
+  );
+}
