c6ce8f8e58b5087e70f337a62bcd50d98eae76f7
Nick Wylynko <nickwylynko@clerk.dev>
2025-03-20 18:44:15 +0800

    reset

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,12 @@
-"use client"
+"use client";
 
-import { SignedIn, SignUpButton, UserProfile } from "@clerk/nextjs";
-import { getUsersOauthToken, getUsersSpotifyCurrentPlaying } from "./actions";
+import { SignedIn, UserProfile } from "@clerk/nextjs";
 
 export default function Home() {
   return (
     <main>
       <SignedIn>
         <UserProfile routing="hash" />
-        <div>
-          <button onClick={async () => console.log(await getUsersOauthToken("spotify"))}>Get users spotify token</button>
-          <button onClick={async () => console.log(await getUsersSpotifyCurrentPlaying())}>Get users currently playing</button>
-        </div>
       </SignedIn>
     </main>
   );
