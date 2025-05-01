21446d97a44361a24c094d8f37349d712c69b2c6
Nick Wylynko <nickwylynko@clerk.dev>
2025-04-30 17:17:08 -0700

    simple setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,11 @@
-"use client"
+import { auth } from "@clerk/nextjs/server";
 
-import { SignedIn, SignUpButton, UserProfile } from "@clerk/nextjs";
-import { getUsersOauthToken, getUsersSpotifyCurrentPlaying } from "./actions";
+export default async function Home() {
+  const user = await auth();
 
-export default function Home() {
   return (
     <main>
-      <SignedIn>
-        <UserProfile routing="hash" />
-        <div>
-          <button onClick={async () => console.log(await getUsersOauthToken("spotify"))}>Get users spotify token</button>
-          <button onClick={async () => console.log(await getUsersSpotifyCurrentPlaying())}>Get users currently playing</button>
-        </div>
-      </SignedIn>
+      <pre>{JSON.stringify(user, null, 2)}</pre>
     </main>
   );
 }
